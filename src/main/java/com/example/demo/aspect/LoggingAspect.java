91da615649cdb463814b220da6da0b899eed70b4
Apangshu121 <apangshu.debnath.accolitedigital.com>
2023-12-25 10:20:09 +0530

    Second Commit

---
+++ src/main/java/com/example/demo/aspect/LoggingAspect.java
@@ -0,0 +1,35 @@
+package com.example.demo.aspect;
+
+import com.example.demo.models.AuditLog;
+import com.example.demo.repository.AuditLogRepository;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class LoggingAspect {
+
+    private final AuditLogRepository auditLogRepository;
+
+    @Async
+    @Around("execution(* com.example.demo.serviceImpl..*(..))")
+    public Object logServiceMethods(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long elapsedTime = System.currentTimeMillis() - start;
+
+        var auditlog = AuditLog.builder()
+                .methodName(joinPoint.getSignature().toString())
+                .executionTime(elapsedTime)
+                .build();
+
+        auditLogRepository.save(auditlog);
+
+        return result;
+    }
+}
---
+++ src/main/java/com/example/demo/models/AuditLog.java
@@ -0,0 +1,23 @@
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "logs")
+public class AuditLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String methodName;
+    private long executionTime;
+
+}
